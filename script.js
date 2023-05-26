b5babb26c217dc486d8d3881dd26abc98d2c4922
santicano <canosantiago404@gmail.com>
2023-05-26 16:59:02 -0300

    Añadido deslizador de imagenes

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ function toggleDropdown(menuId) {
 
     dropdown.style.display = isVisible ? 'none' : 'block';
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000); // Cambiar imagen cada 5 segundos
+}
+
+function changeSlide(n) {
+  slideIndex += n;
+  let slides = document.getElementsByClassName("mySlides");
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  } else if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
